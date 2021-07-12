1b85e810c4e014358d48260eedbc7a2569db38e6
Yadel Negash <yadelnegash10@gmail.com>
2021-07-12 22:01:56 +0000

    added more tests for pages and invaild registration

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -16,6 +16,23 @@ class BasicTests(unittest.TestCase):
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+   
+    def test_home_page(self):
+        response = self.app.get('/home', follow_redirects=True)
+        self.assertEqual(response.status_code, 200) 
+         
+    def test_layout_page(self):
+        response = self.app.get('/layout', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+      
+    def test_register_page(self):
+        response = self.app.get('/register', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
+    def test_second_page(self):
+        response = self.app.get('/second_page', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+      
+      
 if __name__ == "__main__":
     unittest.main()
--- tests/test_user.py
+++ tests/test_user.py
@@ -16,27 +16,30 @@ class UsersTests(unittest.TestCase):
     #### tests ####
     ###############
 
-    def register(self, username, email, password):
+    def register(self, username, email, password, confirm_password):
         return self.app.post('/register',
                             data=dict(username=username,
                                       email=email,
                                       password=password, 
-                                      confirm_password=password),
+                                      confirm_password=confirm_password),
                             follow_redirects=True)
 
     def test_valid_user_registration(self):
-        response = self.register('test', 'test@example.com', 'FlaskIsAwesome')
+        response = self.register('test', 'test@example.com', 'FlaskIsAwesome', 'FlaskIsAwesome')
         self.assertEqual(response.status_code, 200)
          
          
     def test_invalid_username_registration(self):
-        response = self.register('t', 'test@example.com', 'FlaskIsAwesome')
+        response = self.register('t', 'test@example.com', 'FlaskIsAwesome', 'FlaskIsAwesome')
         self.assertIn(b'Field must be between 2 and 20 characters long.', response.data)
-
+      
     def test_invalid_email_registration(self):
-        response = self.register('test2', 'test@example', 'FlaskIsAwesome')
+        response = self.register('test2', 'test@example', 'FlaskIsAwesome', 'FlaskIsAwesome')
         self.assertIn(b'Invalid email address.', response.data)
 
+#     def test_invalid_password_registration(self):
+#         response = self.register('test3', 'test@example.com', 'FlaskIsAwesome', 'FlaskIsNOTAwesome')
+#         self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
