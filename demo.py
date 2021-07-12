5af7a8e6f54749831542e5c37bf37d33c9ca16c9
Yadel Negash <yadelnegash10@gmail.com>
2021-07-12 20:31:29 +0000

    added code to add login infos to the database

--- demo.py
+++ demo.py
@@ -4,12 +4,23 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 import secrets
 from forms import RegistrationForm
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6899aed57fc9a147782e72f9bde1ed9a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+def __repr__(self):
+  return f"User('{self.username}', '{self.email}')"
 
 @app.route("/")
 def hello_world():
@@ -34,10 +45,13 @@ def second_page():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form) 
-
-
+    
+    
 if __name__ == '__main__':
   app.run(debug = True, host = '0.0.0.0')
