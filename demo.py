30f4f8ca92210978dced791fbafdb5c907e5e55d
Yadel Negash <yadelnegash10@gmail.com>
2021-07-12 19:20:36 +0000

    added a registration form

--- demo.py
+++ demo.py
@@ -1,5 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+import secrets
+from forms import RegistrationForm
+# from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '6899aed57fc9a147782e72f9bde1ed9a'
+
 
 @app.route("/")
 def hello_world():
@@ -18,6 +28,15 @@ def layout():
 @app.route('/second_page')
 def second_page():
   return render_template('second_page.html', subtitle = 'Second Page subtitle')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form) 
 
 
 if __name__ == '__main__':
